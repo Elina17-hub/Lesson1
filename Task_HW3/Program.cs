18f3acd84f62453197b5fdf1e3a93b1d366831cc
Elina <elinasarkina17@gmail.com>
2024-01-17 17:53:59 +0100

    add lesson1:task_hw3

---
+++ Task_HW3/Program.cs
@@ -0,0 +1,40 @@
+﻿// 
+
+//задача с 5 гирями и какая самая тяжелая
+
+int n = 5;
+int[] array = { 2, 50, 45, 3, 99 };
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+// тоже самое с for
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+//тоже самое с Foreach
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
